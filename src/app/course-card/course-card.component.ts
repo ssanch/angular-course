785dbb0cf2fe8022edc67348984b58fb82c88455
Salvador Sanchez <salvador@ssanch.com>
2023-04-15 19:47:18 -0600

    My progress

---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,12 @@
+import {Component, Input} from '@angular/core';
+import {Course} from '../model/course';
+
+@Component({
+  selector: 'course-card',
+  templateUrl: './course-card.component.html',
+  styleUrls: ['./course-card.component.css']
+})
+export class CourseCardComponent {
+  @Input()
+  course: Course
+}
